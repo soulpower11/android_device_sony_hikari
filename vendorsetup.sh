6631becbd037cc458689ae3af0efddc0ec30540f
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2018-02-18 21:06:43 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+add_lunch_combo aosp_hikari-eng
 add_lunch_combo aosp_hikari-userdebug
