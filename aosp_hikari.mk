222acddfa00ea25623a67fd21312cca88decf002
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2018-02-18 20:43:25 +0800

    Updated for LineageOS

--- aosp_hikari.mk
+++ aosp_hikari.mk
@@ -1,5 +1,6 @@
 #
-# Copyright 2013 The Android Open-Source Project
+# Copyright (C) 2013-2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,10 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/sony/hikari/full_hikari.mk)
 
+# Device configurations for AOSP
+BOARD_AOSP_BASED := true
+
+# Inherit device configurations
+$(call inherit-product, device/sony/hikari/device.mk)
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device identifications
+PRODUCT_DEVICE := hikari
 PRODUCT_NAME := aosp_hikari
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia arco S
 
-PRODUCT_PACKAGES += \
-    Launcher3
-
+# Build fingerprints
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3204 BUILD_FINGERPRINT=SEMC/LT26w_1266-3204/LT26w:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
